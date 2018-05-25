d314aaabfef785db5f513f6f2c5a2d83cd705aa5
gianmichelesiano <bandigare@gmail.com>
2018-05-25 17:32:14 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase , AngularFireObject} from 'angularfire2/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  gare: Observable<any[]>;
+  gareObject: AngularFireObject<any>;
+
+  constructor(db: AngularFireDatabase) {
+    this.gare = db.list('gare').valueChanges();
+  }
+
+
+chiudi(){
+	console.log("cliudi")
+	sidenav.toggle()
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import { ExComponent } from './ex/ex.component';
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
+
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatCardModule
+} from '@angular/material';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    FlexLayoutModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    AngularFireDatabaseModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
